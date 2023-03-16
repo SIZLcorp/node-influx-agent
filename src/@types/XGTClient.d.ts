a17456309c17807042724e8b75aea23fe517f749
TaeYeong Kim <therichu@gmail.com>
2023-03-16 16:22:12 +0900

    FEATURE: generator 타입스크립트로 재작성 타입들 일부 재작성

--- src/@types/XGTClient.d.ts
+++ src/@types/XGTClient.d.ts
@@ -1,18 +1,12 @@
 // import { NetConnectOpts } from 'net';
 
 declare module "XGTClient" {
-  export enum XGTDataType {
-    'bit',
-    'byte',
-    'word',
-    'dword',
-    'lword',
-    'seq'
-  }
-
+  export type XGTDataType = 'bit' | 'byte' | 'word' | 'dword' | 'lword' | 'seq'
   export type XGTArea = 'P' | 'M' | 'L' | 'F' | 'K' | 'C' | 'D' | 'T' | 'N' | 'R'
-  export type XGTAddressType = `${XGTArea}${number}`
 
+  export type XGTAddressType = `${XGTArea}${number}`
+  export type XGTDataTypeChar = 'X' | 'B' | 'W' | 'D' | 'L'
+  export type XGTProtocolAddressType = `%${XGTArea}${XGTDataTypeChar}${number}`
   export interface XGTClientConfig {
     port: number
     host: string
---
+++ src/xgtClient/generator/write.ts
@@ -0,0 +1,34 @@
+import { XGTProtocolAddressType, XGTDataType } from "XGTClient"
+import { setDataType } from "../util/setDataType"
+
+export default function writeData(address: XGTProtocolAddressType, dataType: XGTDataType, buf: Buffer) {
+  // console.log('buf->', buf)
+  // console.log(buf.length)
+
+  const command = Buffer.from([0x58, 0x00])
+  const dataTypeHEX = setDataType(dataType)
+  const reserved = Buffer.from([0x00, 0x00])
+  const block = Buffer.from([0x01, 0x00])
+  const addr = Buffer.from(address, 'utf8')
+  const addrLength = Buffer.from([addr.length, 0x00])
+  const bufferLength = Buffer.from([buf.length, 0x00])
+  const totalLength = command.length +
+    dataTypeHEX.length +
+    reserved.length +
+    block.length +
+    addrLength.length +
+    addr.length +
+    bufferLength.length + 2
+
+  return Buffer.concat([
+    command,
+    dataTypeHEX,
+    reserved,
+    block,
+    addrLength,
+    addr,
+    bufferLength,
+    buf
+  ], totalLength)
+
+}
