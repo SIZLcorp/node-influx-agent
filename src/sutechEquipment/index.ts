59122ad31b979b23de1266587e5af617e72ba943
TaeYeong Kim <therichu@gmail.com>
2023-03-30 11:54:25 +0900

    FEATURE: env 분리, sutechEquipment 시간 기록 기능 추가

--- src/sutechEquipment/index.ts
+++ src/sutechEquipment/index.ts
@@ -1,32 +1,9 @@
-// 수테크 장비를 모킹함, 장비 내의 메모리맵들 최신값 유지할 수 있도록
-
-// 메모리 맵 설정
-
-
-// f) 데이터 취득 시작 함수
-//  데이터 취득 시작 시각 기록
-//  루프돌면서, 각 메모리 값들 읽어오기
-//  루프 끝나면 데이터 취득 종료
-
-//  q:아직 작동중에 다시 데이터 취득 요청한다면?
-//    a:일단 무시..
-
-// f) 리셋 함수
-// f) 모아진 데이터 출력 함수
-// 인플럭스쪽에서 데이터 출력값 가지고 인플럭스 입력값 만들어야 한다.
-
-// const memoryMap = {
-//   PRODUCT_NUM: {
-//     describe: '생산량',
-//     type: 'int',
-//     address: 'c10'
-//   }
-// }
 import { XGTClient } from '../xgtClient'
 import { XGTAddressType, XGTClientConfig, XGTDataTypeChar } from 'XGTClient'
 import { InfluxCamField, InfluxPressDataField } from 'InfluxClient'
 // import { SutechConfigItem } from "SutechAgent"
 
+// 수테크 장비를 모킹함, 장비 내의 메모리맵들 최신값 유지할 수 있도록
 export interface SutechConfigItem {
   plcAddress: XGTAddressType
   name: string
@@ -37,27 +14,47 @@ export interface SutechConfigItem {
 export class SutechEquipment {
   xgtClient: XGTClient
   memoryMap: SutechConfigItem[]
+  startAt: Date | null = null
+  endAt: Date | null = null
+  isScanning: boolean = false
   memory: any
   constructor(config: XGTClientConfig, memoryMap: SutechConfigItem[]) {
     this.xgtClient = XGTClient.getInstance(config)
     this.memoryMap = memoryMap
     this.reset()
   }
-  async scan() {
+  async scan(): Promise<void> {
+    //  q:아직 작동중에 다시 데이터 취득 요청한다면?
+    //    a:일단 무시..
+    if (this.isScanning) {
+      return
+    }
+
+    // f) 데이터 취득 시작 함수
+    //  데이터 취득 시작 시각 기록
+    //  루프돌면서, 각 메모리 값들 읽어오기
+    //  루프 끝나면 데이터 취득 종료
+
+    this.startAt = new Date()
+    this.isScanning = true
     // memoryMap iteration (Async)
     for (const item of this.memoryMap) {
-      console.log('item', item)
-
       const data = await this.xgtClient.readData(item.plcAddress, item.dataType)
       this.memory = {
         ...this.memory,
         [item.dataCode]: data
       }
     }
+    this.isScanning = false
+    this.endAt = new Date()
   }
-  reset() {
+  // f) 리셋 함수
+  reset(): void {
     this.memory = {
     }
+    this.isScanning = false
+    this.startAt = null
+    this.endAt = null
     // 메모리 맵 보고 초기화
     this.memoryMap.forEach((item) => {
       this.memory = {
@@ -66,7 +63,13 @@ export class SutechEquipment {
       }
     })
   }
+  // f) 모아진 데이터 출력 함수
+  // 인플럭스쪽에서 데이터 출력값 가지고 인플럭스 입력값 만들어야 한다.
   getMemory() {
-    return this.memory
+    return {
+      ...this.memory,
+      startAt: this.startAt,
+      endAt: this.endAt
+    }
   }
 }
