872df596562d7a3e756bf18d7540ff2ea5e51818
TaeYeong Kim <therichu@gmail.com>
2023-04-04 11:37:00 +0900

    FIX: 0 값도 의미있는 값이다 #20

--- src/influxClient/index.ts
+++ src/influxClient/index.ts
@@ -45,88 +45,88 @@ export class InfluxClient {
     point1.intField('version', 1)
     point1.stringField('manufacturer', 'sutech')
 
-    if (data.press_spm) {
+    if (data.press_spm !== null) {
       point1.floatField('press_spm', data.press_spm)
     }
-    if (data.press_angle) {
+    if (data.press_angle !== null) {
       point1.floatField('press_angle', data.press_angle)
     }
-    if (data.press_main_motor_current) {
+    if (data.press_main_motor_current !== null) {
       point1.floatField('press_main_motor_current', data.press_main_motor_current)
     }
-    if (data.press_slide_motor_current) {
+    if (data.press_slide_motor_current !== null) {
       point1.floatField('press_slide_motor_current', data.press_slide_motor_current)
     }
     // 'press_inverter_spm'
-    if (data.press_inverter_spm) {
+    if (data.press_inverter_spm !== null) {
       point1.floatField('press_inverter_spm', data.press_inverter_spm)
     }
     // 'press_preset_counter'
-    if (data.press_preset_counter) {
+    if (data.press_preset_counter !== null) {
       point1.floatField('press_preset_counter', data.press_preset_counter)
     }
     // 'press_total_counter'
-    if (data.press_total_counter) {
+    if (data.press_total_counter !== null) {
       point1.floatField('press_total_counter', data.press_total_counter)
     }
     // 'press_whole_counter'
-    if (data.press_whole_counter) {
+    if (data.press_whole_counter !== null) {
       point1.floatField('press_whole_counter', data.press_whole_counter)
     }
     // 'press_preset_limit_counter'
-    if (data.press_preset_limit_counter) {
+    if (data.press_preset_limit_counter !== null) {
       point1.floatField('press_preset_limit_counter', data.press_preset_limit_counter)
     }
     // 'cycle_index'
-    if (data.cycle_index) {
+    if (data.cycle_index !== null) {
       point1.floatField('cycle_index', data.cycle_index)
     }
     // 'press_safety_one_cycle_stop_angle'
-    if (data.press_safety_one_cycle_stop_angle) {
+    if (data.press_safety_one_cycle_stop_angle !== null) {
       point1.floatField('press_safety_one_cycle_stop_angle', data.press_safety_one_cycle_stop_angle)
     }
     // 'press_safety_one_cycle_slip_angle'
-    if (data.press_safety_one_cycle_slip_angle) {
+    if (data.press_safety_one_cycle_slip_angle !== null) {
       point1.floatField('press_safety_one_cycle_slip_angle', data.press_safety_one_cycle_slip_angle)
     }
     // 'press_key_cam'
-    if (data.press_key_cam) {
+    if (data.press_key_cam !== null) {
       point1.floatField('press_key_cam', data.press_key_cam)
     }
     // 'press_run_ready'
-    if (data.press_run_ready) {
+    if (data.press_run_ready !== null) {
       point1.floatField('press_run_ready', data.press_run_ready)
     }
     // 'press_run_ok'
-    if (data.press_run_ok) {
+    if (data.press_run_ok !== null) {
       point1.floatField('press_run_ok', data.press_run_ok)
     }
     // 'press_motor_state'
-    if (data.press_motor_state) {
+    if (data.press_motor_state !== null) {
       point1.floatField('press_motor_state', data.press_motor_state)
     }
     // 'press_motor_vector'
-    if (data.press_motor_vector) {
+    if (data.press_motor_vector !== null) {
       point1.floatField('press_motor_vector', data.press_motor_vector)
     }
     // 'press_run_state'
-    if (data.press_run_state) {
+    if (data.press_run_state !== null) {
       point1.floatField('press_run_state', data.press_run_state)
     }
     // 'press_error_number'
-    if (data.press_error_number) {
+    if (data.press_error_number !== null) {
       point1.floatField('press_error_number', data.press_error_number)
     }
     // 'press_slide_top_position'
-    if (data.press_slide_top_position) {
+    if (data.press_slide_top_position !== null) {
       point1.floatField('press_slide_top_position', data.press_slide_top_position)
     }
     // 'press_operator_run_time'
-    if (data.press_operator_run_time) {
+    if (data.press_operator_run_time !== null) {
       point1.stringField('press_operator_run_time', data.press_operator_run_time)
     }
     // 'press_operator_stop_time'
-    if (data.press_operator_stop_time) {
+    if (data.press_operator_stop_time !== null) {
       point1.stringField('press_operator_stop_time', data.press_operator_stop_time)
     }
 
