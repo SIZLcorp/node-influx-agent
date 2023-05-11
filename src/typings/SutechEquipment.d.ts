910a895bbcace43e6eb95d0faf72318e96f23872
Juhee <88186482+dormilon24@users.noreply.github.com>
2023-05-11 15:44:32 +0900

    Feature/#37 (#38)
    * FIX: long_field로 저장하기 #37
    
    * FIX: run, stop time convert to HMS #37
    
    * FIX: typo #37
    
    * FIX: 키캠 상태 제스텍과 맞춤 #37
    
    ---------
    
    Co-authored-by: dormilon <dormilon24>

--- src/typings/SutechEquipment.d.ts
+++ src/typings/SutechEquipment.d.ts
@@ -17,6 +17,10 @@ declare module "SutechEquipment" {
     'cycle_index'?: number, // 행정 인덱스
     'press_safety_one_cycle_stop_angle'?: number, // 클러치 정지 각도, f
     'press_safety_one_cycle_slip_angle'?: number, // 클러치 밀림 각도, f
+    'press_key_cam_inching'?: number, // 키캠 상태
+    'press_key_cam_one_cycle'?: number, // 키캠 상태
+    'press_key_cam_continue'?: number, // 키캠 상태
+    'press_key_cam_slide'?: number, // 키캠 상태
     'press_key_cam'?: number, // 키캠 상태
     'press_run_ready'?: number, // 운전 준비 완료
     'press_run_ok'?: number, // 가동 준비 완료
